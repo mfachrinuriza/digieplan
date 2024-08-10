6e742e04c7b71e5efbc13f586b1968048d2a311a
Muhammad Fachri Nuriza <fachri.nuriza@sg-edts.com>
2024-08-10 14:32:15 +0700

    fixing preview and add button change cover image

--- app/Views/customer/commons/layer/sidebar_content.php
+++ app/Views/customer/commons/layer/sidebar_content.php
@@ -98,7 +98,7 @@
             </a>
         </li>
         <li class="nav-item dropdown">
-            <a class="nav-link <?= $title === getenv('TITLE_GUEST') ? "active" : "" ?>" href="<?= base_url('check-preview/' . $transactionSelected['title_path'] . '?to=guest+name%26+keluarga'); ?>" target="_blank">
+            <a class="nav-link <?= $title === getenv('TITLE_GUEST') ? "active" : "" ?>" href="<?= base_url('preview/' . $transactionSelected['title_path'] . '?to=guest+name%26+keluarga'); ?>" target="_blank">
                 <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-eye">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
--- app/Views/customer/wedding/pages/cover_main_setting.php
+++ app/Views/customer/wedding/pages/cover_main_setting.php
@@ -11,17 +11,21 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-4"></div>
-                                <div class="<?= $url_image != null ? "card" : "" ?> col-md-4 p-0" style="aspect-ratio: 8 / 13;">
-                                    <form action="<?= base_url('/customer/kelola-konten-undangan/halaman-utama/create') ?>" method="POST" enctype="multipart/form-data">
-                                        <a class="p-0 m-0 border-0 bg-transparent" onclick="buttonSelectImage()">
-                                            <input type="text" name="transaction_id" id="transaction_id" value="<?= $transactionSelected['id'] ?>" hidden>
-                                            <input type="text" name="type" id="type" value="cover" hidden>
-                                            <input type="file" name="image" id="input-image" onchange="requestSubmitImage()" accept="image/*" hidden>
-                                            <img src="<?= $url_image != null ? base_url('/assets/images/album/' . $url_image) : base_url('/assets/images/icon/default-upload.png') ?>" style="width: 100%; height: 100%; object-fit: cover;">
-                                        </a>
-                                        <button id="button-submit" hidden></button>
-                                    </form>
+                                <div class="col-md-4">
+                                    <div class="<?= $url_image != null ? 'card' : '' ?> p-0 mb-3" style="width: 100%; aspect-ratio: 8 / 13; overflow: hidden;">
+                                        <form action="<?= base_url('/customer/kelola-konten-undangan/halaman-utama/create') ?>" method="POST" enctype="multipart/form-data" style="width: 100%; height: 100%;">
+                                            <a class="p-0 m-0 border-0 bg-transparent" onclick="buttonSelectImage()" style="display: block; width: 100%; height: 100%;">
+                                                <input type="text" name="transaction_id" id="transaction_id" value="<?= $transactionSelected['id'] ?>" hidden>
+                                                <input type="text" name="type" id="type" value="cover" hidden>
+                                                <input type="file" name="image" id="input-image" onchange="requestSubmitImage()" accept="image/*" hidden>
+                                                <img src="<?= $url_image != null ? base_url('/assets/images/album/' . $url_image) : base_url('/assets/images/icon/default-upload.png') ?>" style="width: 100%; height: 100%; object-fit: cover;">
+                                            </a>
+                                            <button id="button-submit" hidden></button>
+                                        </form>
+                                    </div>
+                                    <button class="btn btn-outline-primary w-full" onclick="buttonSelectImage()">Upload Gambar</button>
                                 </div>
+                                <div class="col-md-4"></div>
                             </div>
                         </div>
                     </div>
